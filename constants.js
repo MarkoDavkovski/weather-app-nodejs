fcaeeb208ea65e7c867dcb5df3e7bab499ed59d4
Marko Davkovski <marko.davkovski@gmail.com>
2024-10-27 08:50:46 +0100

    remove constants file

--- constants.js
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://weather-app-nodejs-three.vercel.app/";
